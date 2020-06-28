c59d10bc60ff9427c67f8a3480f136e9511dbff5
sebastianwino <swinog@hotmail.com>
2020-06-27 22:39:13 -0300

    wireframes 2.0, images 2.0

--- app/app.js
+++ app/app.js
@@ -42,7 +42,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('errors', {title: 'Error'});
+  res.render('error', {title: 'Error'});
 });
 
 module.exports = app;
