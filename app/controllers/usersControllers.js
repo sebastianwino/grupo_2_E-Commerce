c13bb991f7979dc5290603704b420e4c38a79d64
sebastianwino <swinog@hotmail.com>
2020-07-06 18:30:07 -0300

    proccesLogin, retrospectiva (pendiente)

--- app/controllers/usersControllers.js
+++ app/controllers/usersControllers.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcrypt');
+
 
 const usersFilePath = path.join(__dirname, '../data/usersDB.json'); 
 
@@ -10,27 +12,38 @@ let usersControllers = {
         res.render('login', {title: 'Login'});
     },
     processLogin: (req, res) => {
-        res.send('Usuario logeado');
+     
+        users.forEach(user => {
+            if (user.email == req.body.email) {
+                if (bcrypt.compareSync(req.body.password, user.password)) {
+                    res.send(`Bienvenido, ${user.name}!!!`);
+                }
+            }
+        })
+        res.send('Usuario no existente, registrate!!!');
+
+        
     },
     register: (req, res) => {
         res.render('register', {title: 'Registrate'});
     },
     create: (req, res) => {
-        let usuario = {
+        let passwordEncripted = bcrypt.hashSync(req.body.password, 10)
+        let newUser = {
             id: users[users.length-1].id+1,
             name: req.body.name,
             lastname: req.body.lastname,
             email: req.body.email,
-            password: req.body.password,
+            password: passwordEncripted,
             address: req.body.address,
             addressNumber: req.body.addressNumber,
             city: req.body.city,
             phoneNumber: req.body.phoneNumber
         };
 
-        users.push(usuario);
+        users.push(newUser);
 
-        let usersJSON = JSON.stringify(users, null, 4);
+        let usersJSON = JSON.stringify(users);
         fs.writeFileSync(usersFilePath, usersJSON);
 
         res.send('Usuario registrado');
