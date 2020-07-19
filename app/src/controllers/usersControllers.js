785f06e993b7deab70f0063dc4b1cc874b346328
sebastianwino <swinog@hotmail.com>
2020-07-18 22:31:03 -0300

    validaciones (a revisar)

--- app/src/controllers/usersControllers.js
+++ app/src/controllers/usersControllers.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcrypt');
 
+const { validationResult } = require('express-validator');
 
 const usersFilePath = path.join(__dirname, '../data/usersDB.json'); 
 
@@ -12,15 +13,24 @@ let usersControllers = {
         res.render('users/login', {title: 'Login'});
     },
     processLogin: (req, res) => {
-     
-        users.forEach(user => {
-            if (user.email == req.body.email) {
-                if (bcrypt.compareSync(req.body.password, user.password)) {
-                    res.send(`Bienvenido, ${user.name}!!!`);
+
+        let errors = validationResult(req)
+
+        if (errors.isEmpty()) {
+            users.forEach(user => {
+
+                /* Decidir que validación se deja (express-validator, la siguiente o ambas) */
+                if (user.email == req.body.email) {
+                    if (bcrypt.compareSync(req.body.password, user.password)) {
+                        res.send(`Bienvenido, ${user.name}!!!`);
+                    }
                 }
-            }
-        })
-        res.send('Usuario no existente, registrate!!!');
+            })    
+        } else {
+            res.render('users/login', {title: 'Login', errors: errors.errors});
+        }
+             
+        
 
         
     },
@@ -28,25 +38,35 @@ let usersControllers = {
         res.render('users/register', {title: 'Registrate'});
     },
     create: (req, res) => {
-        let passwordEncripted = bcrypt.hashSync(req.body.password, 10)
-        let newUser = {
-            id: users[users.length-1].id+1,
-            name: req.body.name,
-            lastname: req.body.lastname,
-            email: req.body.email,
-            password: passwordEncripted,
-            address: req.body.address,
-            addressNumber: req.body.addressNumber,
-            city: req.body.city,
-            phoneNumber: req.body.phoneNumber
-        };
+        let errors = validationResult(req)
 
-        users.push(newUser);
+        if (errors.isEmpty()) {
 
-        let usersJSON = JSON.stringify(users);
-        fs.writeFileSync(usersFilePath, usersJSON);
+            let passwordEncripted = bcrypt.hashSync(req.body.password, 10)
+            let newUser = {
+                id: users[users.length-1].id+1,
+                name: req.body.name,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                password: passwordEncripted
+                /* address: req.body.address,
+                addressNumber: req.body.addressNumber,
+                city: req.body.city,
+                phoneNumber: req.body.phoneNumber */
+            };
 
-        res.send('Usuario registrado');
+            users.push(newUser);
+
+            let usersJSON = JSON.stringify(users);
+            fs.writeFileSync(usersFilePath, usersJSON);
+
+            res.send('Usuario registrado');
+
+        } else {
+            res.render('users/register', {title: 'Registrate', errors: errors.errors});
+        }
+
+        
     }
 }
 
