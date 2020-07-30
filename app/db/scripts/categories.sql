b08cd170e51a381dc67c4a85340ef3e38ab123ca
sebastianwino <swinog@hotmail.com>
2020-07-30 15:54:44 -0300

    scripts

---
+++ app/db/scripts/categories.sql
@@ -0,0 +1,6 @@
+insert into categories (id, name, created_at) values (1, 'tristique', '2020-07-29 00:00:00');
+insert into categories (id, name, created_at) values (2, 'vestibulum velit id', '2020-07-29 00:00:00');
+insert into categories (id, name, created_at) values (3, 'sit', '2020-07-29 00:00:00');
+insert into categories (id, name, created_at) values (4, 'cubilia', '2020-07-29 00:00:00');
+insert into categories (id, name, created_at) values (5, 'donec ut', '2020-07-29 00:00:00');
+insert into categories (id, name, created_at) values (6, 'lobortis ligula sit', '2020-07-29 00:00:00');
