c377fff884f4af04c5c21227cee0c859300d3b06
JuanOctavioCastrogiovanni <Jcastrogiovanni@est.frba.utn.edu.ar>
2020-08-21 15:11:17 -0300

    agregando cosas al DOM de detalle

--- app/public/js/products/detailProducts.js
+++ app/public/js/products/detailProducts.js
@@ -1,3 +1,24 @@
+window.onload = ()=> {
+const image = document.querySelector('.image');
+
+image.addEventListener('mousemove', function (e) {
+ let width = image.offsetWidth;
+ let height = image.offsetHeight;
+ let mouseX = e.offsetX;
+ let mouseY = e.offsetY;
+ 
+ let bgPosX = (mouseX / width * 100);
+ let bgPosY = (mouseY / height * 100);
+ 
+ image.style.backgroundPosition = `${bgPosX}% ${bgPosY}%`;
+});
+
+image.addEventListener('mouseleave', function () {
+ image.style.backgroundPosition = "center";
+});
+
+
+}
 // window.onload = ()=> {
 // let image = document.querySelector('img.image-prince')
 
