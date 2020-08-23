6ed3cd86ac156db8a2e92dd1c833d4a29827d96a
JuanOctavioCastrogiovanni <Jcastrogiovanni@est.frba.utn.edu.ar>
2020-08-23 20:31:24 -0300

    modularizando galeria

---
+++ app/src/db/factories/usersFactory.js
@@ -0,0 +1,15 @@
+// const faker = require('faker')
+// const bcrypt = require('bcrypt')
+
+// module.exports = (n = 1) => {
+//     let users = []
+
+//     for (let i = 0; i < n; i++) {
+//       users.push({
+//         email: faker.internet.email(),
+//         password: bcrypt.hashSync('secret', 10)
+//       })
+//     }
+
+//     return users
+// }
