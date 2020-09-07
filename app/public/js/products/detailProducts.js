0b8343220062262c96deb0992c8b0adf585d6ad9
juanOctavioCastrogiovanni <Jcastrogiovanni@est.frba.utn.edu.ar>
2020-09-07 11:49:25 -0300

    pre-entrega v.2

--- app/public/js/products/detailProducts.js
+++ app/public/js/products/detailProducts.js
@@ -1,60 +1,60 @@
 window.onload = () => {
-    // let btn = document.getElementById('userBtn')
-    // let animation = document.getElementById('userBtnClick')
-    // let body = document.querySelectorAll('.h')
-    // let detras = document.getElementById('detras')
-    // let delante = document.getElementById('delante')
-    // var mediaqueryList = window.matchMedia("(max-width: 992px)");
-    // let flag = false;
-    // let flag2 = false;
+    let btn = document.getElementById('userBtn')
+    let animation = document.getElementById('userBtnClick')
+    let body = document.querySelectorAll('.h')
+    let detras = document.getElementById('detras')
+    let delante = document.getElementById('delante')
+    var mediaqueryList = window.matchMedia("(max-width: 992px)");
+    let flag = false;
+    let flag2 = false;
 
 
 
 
-    // mediaqueryList.addListener(function (EventoMediaQueryList) {
-    //     console.log('Ejecutado el listener', EventoMediaQueryList.matches);
-    //     document.addEventListener('scroll', () => {
-    //         const scrolled = scrollY
-    //         console.log(scrolled)
-    //         console.log(EventoMediaQueryList.matches)   
-    //         if (EventoMediaQueryList.matches) {
-    //             if (scrolled < 215) {
-    //                 detras.classList.add('d-none')
-    //                 delante.classList.remove('d-none')
-    //             } else {
-    //                 detras.classList.remove('d-none')
-    //                 delante.classList.add('d-none')
-    //             }
-    //         } else {
-    //             if (scrolled < 330) {
-    //                 detras.classList.add('d-none')
-    //                 delante.classList.remove('d-none')
-    //             } else {
-    //                 detras.classList.remove('d-none')
-    //                 delante.classList.add('d-none')
-    //             }
-    //         }
-    //     })
-    // });
+    mediaqueryList.addListener(function (EventoMediaQueryList) {
+        console.log('Ejecutado el listener', EventoMediaQueryList.matches);
+        document.addEventListener('scroll', () => {
+            const scrolled = scrollY
+            console.log(scrolled)
+            console.log(EventoMediaQueryList.matches)
+            if (EventoMediaQueryList.matches) {
+                if (scrolled < 215) {
+                    detras.classList.add('d-none')
+                    delante.classList.remove('d-none')
+                } else {
+                    detras.classList.remove('d-none')
+                    delante.classList.add('d-none')
+                }
+            } else {
+                if (scrolled < 330) {
+                    detras.classList.add('d-none')
+                    delante.classList.remove('d-none')
+                } else {
+                    detras.classList.remove('d-none')
+                    delante.classList.add('d-none')
+                }
+            }
+        })
+    });
 
-    // document.addEventListener('scroll', () => {
-    //     const scrolled = scrollY
-    //     console.log(scrolled)
-    //     if (scrolled < 215) {
-    //             detras.classList.add('d-none')
-    //             delante.classList.remove('d-none')
-    // } else {
-    //             detras.classList.remove('d-none')
-    //             delante.classList.add('d-none')
-    //         }
-    //     if (scrolled < 330) {
-    //             detras.classList.add('d-none')
-    //             delante.classList.remove('d-none')
-    //     } else {
-    //             detras.classList.remove('d-none')
-    //             delante.classList.add('d-none')
-    //     }
-    // })
+    document.addEventListener('scroll', () => {
+        const scrolled = scrollY
+        console.log(scrolled)
+        if (scrolled < 215) {
+                detras.classList.add('d-none')
+                delante.classList.remove('d-none')
+    } else {
+                detras.classList.remove('d-none')
+                delante.classList.add('d-none')
+            }
+        if (scrolled < 330) {
+                detras.classList.add('d-none')
+                delante.classList.remove('d-none')
+        } else {
+                detras.classList.remove('d-none')
+                delante.classList.add('d-none')
+        }
+    })
 
 
 
@@ -68,7 +68,7 @@ window.onload = () => {
 
         let bgPosX = (mouseX / width * 100);
         let bgPosY = (mouseY / height * 100);
-
+      
         image.style.backgroundPosition = `${bgPosX}% ${bgPosY}%`;
     });
 
@@ -80,45 +80,38 @@ window.onload = () => {
     // animacion boton usuario
 
 
-    // function close () {
-    //     animation.classList.add('hiddenAnimation')
-    //     animation.classList.remove('playing')
-    //     flag = true;
-    //     animation.style.height = "0"
-    //     animation.style.width = "0"
-    //     flag2=false
+    function close () {
+        animation.classList.add('hiddenAnimation')
+        animation.classList.remove('playing')
+        flag = true;
+        animation.style.height = "0"
+        animation.style.width = "0"
+        flag2=false
 
-    // }
+    }
 
-    // btn.addEventListener('click', () => {
-    // if (!flag2) {
-    // animation.classList.add('playing')
-    // animation.style.height = "177px"
-    // animation.style.width = "150px"
-    //     if (flag) {
-    //         animation.classList.remove('hiddenAnimation')
-    //     }
-    //    flag2=true
-    // } else {
-    //     close ()
-    // } 
-    // })
-
-
-    // for (let i=0; i<=body.length;i++) {
-    // body[i].addEventListener('mouseover', () => {
-    //     if (flag2) {
-    //       close() 
-    //     }
-    // })
-    // }
+    btn.addEventListener('click', () => {
+    if (!flag2) {
+    animation.classList.add('playing')
+    animation.style.height = "177px"
+    animation.style.width = "150px"
+        if (flag) {
+            animation.classList.remove('hiddenAnimation')
+        }
+       flag2=true
+    } else {
+        close ()
+    }
+    })
 
 
-
-
-
-
-
+    for (let i=0; i<=body.length;i++) {
+    body[i].addEventListener('mouseover', () => {
+        if (flag2) {
+          close()
+        }
+    })
+    }
 }
 // window.onload = () => {
 // let image = document.querySelector('img.image-prince')
